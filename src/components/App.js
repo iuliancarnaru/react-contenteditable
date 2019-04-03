f566e71f0f1c6dfdfe7cdfda87f0d2848ada3800
Iulian Carnaru <iulian.carnaru@immediate.co.uk>
2019-04-03 15:11:02 +0100

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "../style/App.scss";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>React Boilerplate</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
